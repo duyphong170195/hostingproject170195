dbb309e424874a87fdce7c2da096819eb3c12aa4
phongnd <phongnd.it@tripi.vn>
2021-03-05 15:25:45 +0700

    say Hello

---
+++ src/main/java/com/example/hostingproject170195/Hostingproject170195Application.java
@@ -0,0 +1,13 @@
+package com.example.hostingproject170195;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hostingproject170195Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hostingproject170195Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hostingproject170195/Hostingproject170195ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostingproject170195;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hostingproject170195ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
